62eb9b4fc06fb7da3cbc5ff480be93e47bdd9fae
ihurrahi <andylau1991@gmail.com>
2018-08-26 00:50:49 -0700

    [thehunt] add month labels for stage 8

--- root/var/www/js/thehunt.js
+++ root/var/www/js/thehunt.js
@@ -516,6 +516,20 @@ ${createSubmitForm(8)}
 function setStageNine() {
   var page = `
 <div>
+  <div id="months">
+    <div>J</div>
+    <div>F</div>
+    <div>M</div>
+    <div>A</div>
+    <div>M</div>
+    <div>J</div>
+    <div>J</div>
+    <div>A</div>
+    <div>S</div>
+    <div>O</div>
+    <div>N</div>
+    <div>D</div>
+  </div>
   <div id="grid">
     <div id="inner-grid">
     </div>
