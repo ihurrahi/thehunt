dfdd006f856690766404ccae6cae1ec041f4f842
Andy Lau <andy@thinkingalaud.com>
2018-12-30 17:05:03 -0800

    changes to deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 WEBSITE_DIR=~/Documents/thinkingalaudwebsite/
 SSH_KEY=~/.ssh/google_compute_engine
 USER=andyl
-INSTANCE_IP=35.227.35.14
+INSTANCE_IP=35.237.99.26
 INSTANCE_NAME=instance-1
 
 cd $WEBSITE_DIR
 rsync -vaz -e "ssh -i $SSH_KEY" --exclude-from=.rsync-exclude --delete $WEBSITE_DIR/root/ $USER@$INSTANCE_IP:~/website_staging/
 rsync -vaz -e "ssh -i $SSH_KEY" --exclude-from=.rsync-exclude --delete $WEBSITE_DIR/instance_deploy.bash $USER@$INSTANCE_IP:~
-gcloud compute ssh $INSTANCE_NAME --command="bash instance_deploy.bash"
+gcloud compute ssh $USER@$INSTANCE_NAME --command="bash instance_deploy.bash"
 
